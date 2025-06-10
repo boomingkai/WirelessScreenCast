aabafb0d37669fcef6dfbd1a1df6f0ce583cd2e7
GK-PC\kai.guo <gkmails@126.com>
2025-06-10 20:21:46 +0800

    add  ffmpeg library

--- CastSenderCore/CastSenderCore.cpp
+++ CastSenderCore/CastSenderCore.cpp
@@ -1,5 +1,11 @@
 #include "CastSenderCore.h"
 #include "../LogModule/Loglib.h"
+extern "C" {
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+#include "libavutil/avutil.h"
+}
+
 
 bool Init()
 {
